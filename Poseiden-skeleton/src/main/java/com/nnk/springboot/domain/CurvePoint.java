e000973e45f2680f5937ff3a42ddf09424d4d6bc
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-03-20 14:46:39 +0100

    Fill CurvePoint model

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -5,11 +5,70 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import java.sql.Date;
 import java.sql.Timestamp;
 
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+    private Integer curveId;
+    
+    private Date asOfDate;
+    
+    private Double term;
+    
+    private Double value;
+    
+    private Date creationDate;
+	
+    public CurvePoint(Integer curveId, Double term, Double value) {
+		this.curveId = curveId;
+		this.term = term;
+		this.value = value;
+	}
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCurveId() {
+		return curveId;
+	}
+	public void setCurveId(Integer curveId) {
+		this.curveId = curveId;
+	}
+	public Date getAsOfDate() {
+		return asOfDate;
+	}
+	public void setAsOfDate(Date asOfDate) {
+		this.asOfDate = asOfDate;
+	}
+	public Double getTerm() {
+		return term;
+	}
+	public void setTerm(Double term) {
+		this.term = term;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+    
 }
