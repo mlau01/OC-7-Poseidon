d84b2c7de0867296219a47a7586487854f2f32df
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-03-26 18:39:05 +0100

    Implement edit/delete features

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -53,20 +53,31 @@ public class RuleNameController {
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+    	log.info("GET Request to /ruleName/update/" + id);
+        RuleName ruleName = ruleNameService.findById(id);
+        if(ruleName != null) {
+        	model.addAttribute("ruleName", ruleName);
+        }
         return "ruleName/update";
     }
 
     @PostMapping("/ruleName/update/{id}")
     public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+    	log.info("POST Request to /ruleName/update/" + id);
+        if(! result.hasErrors()) {
+        	ruleNameService.save(ruleName);
+        }
+        else {
+        	return "ruleName/update";
+        }
         return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+    	log.info("GET Request to /ruleName/delete/" + id);
+        ruleNameService.delete(id);
         return "redirect:/ruleName/list";
     }
 }
