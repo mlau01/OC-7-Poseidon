260741642c914a8c7da6403e7a80b64b9c7e5ce8
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-03-26 18:33:55 +0100

    Add list and add controller/view

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,6 +1,11 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.services.IRuleNameService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,24 +18,37 @@ import javax.validation.Valid;
 
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+    
+	private static Logger log = LoggerFactory.getLogger(RuleNameController.class);
+	
+	@Autowired
+	private IRuleNameService ruleNameService;
 
     @RequestMapping("/ruleName/list")
     public String home(Model model)
     {
-        // TODO: find all RuleName, add to model
+    	log.info("GET Request to /ruleName/list");
+        model.addAttribute("ruleNameList", ruleNameService.findAll());
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
     public String addRuleForm(RuleName bid) {
+    	log.info("GET Request to /ruleName/add");
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
     public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
+    	log.info("POST Request to /ruleName/validate");
+        if(! result.hasErrors()) {
+        	ruleNameService.save(ruleName);
+        }
+        else {
+        	return "ruleName/add";
+        }
+        
+        return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/update/{id}")
