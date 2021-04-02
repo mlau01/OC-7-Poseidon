2f2053bbc5aa2993bb934f1728598242cd08dd17
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-04-02 15:21:27 +0200

    Complete user controller test calling service

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/UserControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/UserControllerTest.java
@@ -1,7 +1,8 @@
 package com.nnk.springboot;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
@@ -10,7 +11,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,8 +18,6 @@ import org.springframework.validation.BindingResult;
 import com.nnk.springboot.controllers.UserController;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.services.IUserService;
-
-import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
@@ -44,7 +42,7 @@ public class UserControllerTest {
 	}
 	
 	@Test
-	public void userServiceListTest_shouldCallServiceList() {
+	public void userControllerListTest_shouldCallServiceList() {
 		when(userService.findAll()).thenReturn(null);
 		
 		controller.home(model);
@@ -53,12 +51,38 @@ public class UserControllerTest {
 	}
 	
 	@Test
-	public void userServiceValidateAddTest_shouldCallServiceSave() throws PasswordPatternException, UsernameExistException {
+	public void userControllerValidateAddTest_shouldCallServiceSave() throws PasswordPatternException, UsernameExistException {
 		when(userService.save(any(User.class))).thenReturn(null);
 		
 		controller.validate(user, bindingResult, model);
 		
 		verify(userService, Mockito.times(1)).save(any(User.class));
 	}
-
+	
+	@Test
+	public void userControllerUpdateFormTest_shouldCallServiceUpdateForm()  {
+		when(userService.findById(anyInt())).thenReturn(user);
+		
+		controller.showUpdateForm(anyInt(), model);
+		
+		verify(userService, Mockito.times(1)).findById(anyInt());
+	}
+	
+	@Test
+	public void userControllerUpdateTest_shouldCallServiceUpdateUser() throws PasswordPatternException, UsernameExistException  {
+		when(userService.save(user)).thenReturn(user);
+		
+		controller.updateUser(0, user, bindingResult, model);
+		
+		verify(userService, Mockito.times(1)).save(user);
+	}
+	
+	@Test
+	public void userControllereDeleteTest_shouldCallServiceUpdateUser() throws PasswordPatternException, UsernameExistException  {
+		when(userService.findById(anyInt())).thenReturn(user);
+		
+		controller.deleteUser(0, model);
+		
+		verify(userService, Mockito.times(1)).delete(user);
+	}
 }
